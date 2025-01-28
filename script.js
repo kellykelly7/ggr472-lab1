e3c4e2057c86ebcd1c6b643f783d1f43d4675f0b
kellykelly7 <kellytruong519@gmail.com>
2025-01-28 17:08:22 -0500

    stylistic edits and nav bar

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ const bttn = document.getElementById("places_to_eat");
 bttn.addEventListener("click", () => {
     window.open("map.geojson", "_blank");
 });
+
+const acorn = document.getElementById("acorn");
+const quercus = document.getElementById("quercus");
+const artsci_cal = document.getElementById("artsci_calendar");
+const ttb = document.getElementById("timetable_builder");
+const utl = document.getElementById("utlibraries");
+
+acorn.addEventListener("click", () => {
+    window.open("https://acorn.utoronto.ca/", "_blank");
+});
+
+quercus.addEventListener("click", () => {
+    window.open("https://q.utoronto.ca/", "_blank");
+});
+
+artsci_cal.addEventListener("click", () => {
+    window.open("https://artsci.calendar.utoronto.ca/", "_blank");
+});
+
+ttb.addEventListener("click", () => {
+    window.open("https://ttb.utoronto.ca/", "_blank");
+});
+
+utl.addEventListener("click", () => {
+    window.open("https://onesearch.library.utoronto.ca/", "_blank");
+});
