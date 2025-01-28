b3e969c83a94ef15d4282541285efa366fbbf73a
kellykelly7 <kellytruong519@gmail.com>
2025-01-28 01:17:16 -0500

    rehaul with better understanding of css and html

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const btn = document.getElementById("acorn");
+const btn = document.getElementById("sidsmith_commons");
 
 btn.addEventListener("click", () => {
-    window.open("https://acorn.utoronto.ca", "_blank");
+    window.open("https://sidneysmithcommons.artsci.utoronto.ca/", "_blank");
+});
+
+const bttn = document.getElementById("places_to_eat");
+
+bttn.addEventListener("click", () => {
+    window.open("map.geojson", "_blank");
 });
