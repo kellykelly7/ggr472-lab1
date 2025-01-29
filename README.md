7a7f43b511a60ae686686c4f3af692fce62f99e3
kellykelly7 <kellytruong519@gmail.com>
2025-01-28 23:08:40 -0500

    added comments and image source to readme file
    comments added in all three codes (js, css, html)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ggr472-lab1
- lab 1 for ggr472 
+Lab 1 for GGR472 
+Image in webpage was taken from the Faculty of Arts and Science of the University of Toronto website.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// establishing constant variables for all my buttons from the html page with IDs
 const btn = document.getElementById("sidsmith_commons");
 const bttn = document.getElementById("places_to_eat");
 const acorn = document.getElementById("acorn");
@@ -6,7 +7,9 @@ const artsci_cal = document.getElementById("artsci_calendar");
 const ttb = document.getElementById("timetable_builder");
 const utl = document.getElementById("utlibraries");
 
+// adding a reaction to clicking on the buttons added to the html webpage
 btn.addEventListener("click", () => {
+    // opens a new window with the provided link, that is different per variable
     window.open("https://sidneysmithcommons.artsci.utoronto.ca/", "_blank");
 });
 
